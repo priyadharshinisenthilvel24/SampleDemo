2fd8e4d23779e35cc77085849b3c882cd60fbe95
unknown <priyadharshinisenthilvel24>
2022-06-22 17:03:16 +0530

    initial files

---
+++ src/test/java/com/ideas2it/userandrolemodule/UserandrolemoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas2it.userandrolemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserandrolemoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
