4f81ea2ac2d81b6b71357047422e99a8a6d204f4
Priyadharshinisenthilvel24 <Priyadharshinisenthilvel24>
2022-06-24 19:00:34 +0530

    Added controller class and repository class

--- src/main/java/com/ideas2it/userandrolemodule/controller/UserRoleController.java
+++ src/main/java/com/ideas2it/userandrolemodule/controller/UserRoleController.java
@@ -1,4 +1,50 @@
 package com.ideas2it.userandrolemodule.controller;
 
+import com.ideas2it.userandrolemodule.entity.UserSchema;
+import com.ideas2it.userandrolemodule.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+//import javax.validation.Valid;
+
+import java.util.List;
+
+@RestController
 public class UserRoleController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user")
+    public UserSchema saveUserSchema(
+            /*@Valid*/ @RequestBody UserSchema userSchema) {
+        return userService.saveUser(userSchema);
+    }
+
+    //get userid
+    @GetMapping("/user")
+    public List<UserSchema> getUserList() {
+        return userService.getUserList();
+    }
+
+    /*@GetMapping("/user/{id}")
+    public List<UserSchema> getByUserId() {
+        return userService.getByUserId();
+    }*/
+
+    // Update userid
+    @PutMapping("/user/{id}")
+    public UserSchema updateUser(@RequestBody UserSchema userSchema,
+                     @PathVariable("id") String userId) {
+        return userService.updateUser(
+                userSchema, userId);
+    }
+
+    // Delete user by id
+    @DeleteMapping("/user/{id}")
+    public String deleteUserById(@PathVariable("id")
+                                       String userId) {
+        userService.deleteUserById(
+                userId);
+        return "Deleted Successfully";
+    }
 }
--- src/main/java/com/ideas2it/userandrolemodule/repository/UserRepository.java
+++ src/main/java/com/ideas2it/userandrolemodule/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.ideas2it.userandrolemodule.repository;
 
-public interface UserRepository {
+import com.ideas2it.userandrolemodule.entity.UserSchema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserSchema, String>{
 }
