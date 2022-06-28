c67516b3dd1bc7a890eb7c7bb0c8fec6cb7fa206
Priyadharshinisenthilvel24 <Priyadharshinisenthilvel24>
2022-06-28 12:08:47 +0530

    uri modified

--- src/main/java/com/ideas2it/userandrolemodule/controller/UserRoleController.java
+++ src/main/java/com/ideas2it/userandrolemodule/controller/UserRoleController.java
@@ -3,10 +3,13 @@ package com.ideas2it.userandrolemodule.controller;
 import com.ideas2it.userandrolemodule.entity.UserSchema;
 import com.ideas2it.userandrolemodule.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 //import javax.validation.Valid;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class UserRoleController {
@@ -14,25 +17,32 @@ public class UserRoleController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/user")
+    @PostMapping("/users")
     public UserSchema saveUserSchema(
             /*@Valid*/ @RequestBody UserSchema userSchema) {
         return userService.saveUser(userSchema);
     }
 
     //get userid
-    @GetMapping("/user")
+    @GetMapping("/users")
     public List<UserSchema> getUserList() {
         return userService.getUserList();
     }
 
-    /*@GetMapping("/user/{id}")
-    public List<UserSchema> getByUserId() {
-        return userService.getByUserId();
-    }*/
+    @GetMapping("/users/{id}")
+    @ResponseBody
+    public ResponseEntity<UserSchema> getByUserId(@PathVariable("id") String userId) {
+        try {
+            UserSchema userSchema = userService.getByUserId(userId);
+            return new ResponseEntity<>(userSchema, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     // Update userid
-    @PutMapping("/user/{id}")
+    @PutMapping("/users/{id}")
     public UserSchema updateUser(@RequestBody UserSchema userSchema,
                      @PathVariable("id") String userId) {
         return userService.updateUser(
@@ -40,9 +50,8 @@ public class UserRoleController {
     }
 
     // Delete user by id
-    @DeleteMapping("/user/{id}")
-    public String deleteUserById(@PathVariable("id")
-                                       String userId) {
+    @DeleteMapping("/users/{id}")
+    public String deleteUserById(@PathVariable("id") String userId) {
         userService.deleteUserById(
                 userId);
         return "Deleted Successfully";
